5d8f25e6835d1198405f3f98e4cbda388585d9ea
cmtlyt <cmtlyt@163.com>
2024-08-07 19:42:41 +0800

    chore: 升级vitest

--- packages/base/vitest.config.ts
+++ packages/base/vitest.config.ts
@@ -1,11 +1,5 @@
-import { resolve } from 'node:path';
 import { defineProject } from 'vitest/config';
 
 export default defineProject({
-  test: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-      '@com': resolve(__dirname, 'src/common'),
-    },
-  },
+  test: {},
 });
