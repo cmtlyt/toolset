b599ecef43cef143e13463c134c3c6eca39125fa
cmtlyt <cmt01964896@taobao.com>
2025-01-17 08:58:37 +0800

    fix: 模板构建命令调整

--- packages/create-origin/template-source/build-template.ts
+++ packages/create-origin/template-source/build-template.ts
@@ -21,12 +21,12 @@ glob('./**/*.ts', { cwd: __dirname, absolute: true }).then((paths) => {
       return;
     const sourcePath = filePath;
     const config = (await import(sourcePath)).default;
-    const fileName = sourcePath.split(/[\\/]/).slice(-2).join('/').replace(/\.ts$/, '.json');
+    const fileName = sourcePath.split(/[\\/]/).slice(-2).join(path.sep).replace(/\.ts$/, '.json');
     const targetDir = path.resolve(templatePath, path.dirname(fileName));
     fs.existsSync(targetDir) || fs.mkdirSync(targetDir, { recursive: true });
     const targetPath = path.resolve(templatePath, fileName);
     const json = JSON.stringify(config);
-    if (!ignoreLint.includes(fileName)) {
+    if (!ignoreLint.includes(fileName.replaceAll(path.sep, '/'))) {
       const err = lint(json);
       if (err) {
         console.error(filePath, err, fileName);
